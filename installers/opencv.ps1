b6f7722510629ed355f3951bbb14645aeb329dd2
Akira Shimose <killerofzom33@gmail.com>
2024-08-01 20:36:43 +0700

    switch to MSVC and added automatic detection of the repo root folder

--- installers/opencv.ps1
+++ installers/opencv.ps1
@@ -1,17 +1,26 @@
+# Invoke-WebRequest -Uri https://github.com/microsoft/winget-cli/releases/download/v1.3.2691/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -OutFile .\MicrosoftDesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+winget install Microsoft.VisualStudio.2022.BuildTools #make sure to also select desktop developement with C++
+# uncomment the first line if you dont have winget installed for some reason
+
 wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/4.x.zip
 wget -O opencv.zip https://github.com/opencv/opencv/archive/4.x.zip
 Expand-Archive -Path ./opencv.zip
 Expand-Archive -Path ./opencv_contrib.zip
-#downloading opencv and extracting it
+downloading required stuff and extracting it
+cd ../
 
-New-Item -ItemType Directory -Path ./build
-set-location ./build
+$P_REPO_LOCATION = Get-Location
+
+New-Item -ItemType Directory -Path ./installers/build
+set-location ./installers/build
+
 
 $CMAKE_OPTIONS = '-DBUILD_PERF_TESTS:BOOL=OFF -DBUILD_TESTS:BOOL=OFF -DBUILD_DOCS:BOOL=OFF  -DWITH_CUDA:BOOL=OFF -DBUILD_EXAMPLES:BOOL=OFF -DINSTALL_CREATE_DISTRIB=ON'
-$CMAKE_GENERATOR_OPTIONS = '-G "Ninja"' #switch to whatever generator you use but the ones with long names is not working for some stupid reason
-$P_REPO_LOCATION = 'C:\Users\ourpc\CLionProjects\Polarity-' #change ya path to whereever polarity root folder is
+$CMAKE_GENERATOR_OPTIONS = '-G Visual Studio 17 2022'
+
 
 Write-host "$CMAKE_OPTIONS"
 Write-host "$CMAKE_GENERATOR_OPTIONS"
-Write-host "$REPO_LOCATION"
+Write-host "$P_REPO_LOCATION" #for verification purposes
+
 cmake "$CMAKE_GENERATOR_OPTIONS" "$CMAKE_OPTIONS" -DOPENCV_EXTRA_MODULES_PATH=../opencv_contrib/opencv_contrib-4.x/modules -DCMAKE_INSTALL_PREFIX="$P_REPO_LOCATION/opencv" "../opencv/opencv-4.x"
